c7da6a428d1078931efc2cd62c08e3019a0dab09
Dima <dimabojko93@gmail.com>
2020-11-02 21:53:29 +0200

    add portfolio

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-let menu = document.querySelector(".menu"),
-	menuItem = document.querySelectorAll(".menu__item"),
-	nav = document.querySelector(".nav"),
-	burgerMenu = document.querySelector(".burger-menu"),
-	burgerMenuItem = document.querySelectorAll(".burger-menu__item");
+"use strict";
 
-document.addEventListener("click", function(event) {
-	if (event.target && event.target.classList.contains("nav") 
-		|| event.target.classList.contains("burger-menu__item") || event.target.classList.contains("burger-menu") ) {
+document.addEventListener("DOMContentLoaded", () => {
 
-		burgerMenuItem[0].classList.toggle("top-transform");
-		burgerMenuItem[1].classList.toggle("center-transform");
-		burgerMenuItem[2].classList.toggle("bottom-bg");
-		nav.classList.toggle("hide");
-	}
+// 	const greeting = document.querySelector(".greeting"),
+// 		  	pageWrap = document.querySelector(".page-wrapper");	
+
+
+// function hideContent() {
+// 	pageWrap.classList.toggle("hide");
+// }
+
+
+
 })
