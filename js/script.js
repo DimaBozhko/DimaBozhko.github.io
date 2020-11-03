67d70628876e642b18f4ec90835f43072c6a7a69
Dima <dimabojko93@gmail.com>
2020-11-03 19:19:08 +0200

    test6

--- js/script.js
+++ js/script.js
@@ -1,15 +1,62 @@
-"use strict";
+let menu = document.querySelector(".menu"),
+	menuItem = document.querySelectorAll(".menu__item"),
+	nav = document.querySelector(".nav"),
+	burgerMenu = document.querySelector(".burger-menu"),
+	burgerMenuItem = document.querySelectorAll(".burger-menu__item");
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("click", function(event) {
+	if (event.target && event.target.classList.contains("nav") 
+		|| event.target.classList.contains("burger-menu__item") || event.target.classList.contains("burger-menu") ) {
 
-// 	const greeting = document.querySelector(".greeting"),
-// 		  	pageWrap = document.querySelector(".page-wrapper");	
-
-
-// function hideContent() {
-// 	pageWrap.classList.toggle("hide");
-// }
-
-
-
+		burgerMenuItem[0].classList.toggle("top-transform");
+		burgerMenuItem[1].classList.toggle("center-transform");
+		burgerMenuItem[2].classList.toggle("bottom-bg");
+		nav.classList.toggle("hide");
+	}
 })
+
+//--slider--
+
+let sliderImg = document.querySelectorAll(".slider__img"),
+	afterBtn = document.querySelector(".after-btn"),
+	beforeBtn = document.querySelector(".before-btn");
+
+//--radio-button--
+
+let form = document.querySelector(".form"),
+	wrapper = document.querySelector(".wrapper"),
+	// --Program variables--
+	programWrapper = document.querySelectorAll(".program-wrapper-js"),
+	programText = document.querySelectorAll(".program-text-js"),
+	programInput = document.querySelectorAll(".program-input-js"),
+	circle = document.querySelectorAll(".circle"),
+	// --Extra variables--
+	extraWrapper = document.querySelectorAll(".extra-wrapper-js"),
+	extraText = document.querySelectorAll(".extra-text-js"),
+	extraInput = document.querySelectorAll(".extra-input-js"),
+	checkMark = document.querySelectorAll(".check-mark");
+
+function hideCircle(e) {
+	circle.forEach(item => {
+		item.style.opacity = "0";
+	})
+}
+
+programInput.forEach((item, i) => {
+	item.addEventListener("change", (e) => {
+		hideCircle();
+		if (e.target == item) {
+			circle[i].style.opacity = "1";
+		}
+	})
+})
+
+extraInput.forEach((item, i) => {
+	item.addEventListener("change", (e) => {
+		if (e.target == item) {
+			checkMark[i].classList.toggle("check-mark-hide")
+		}
+	})
+})
+
+
