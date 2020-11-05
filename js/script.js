246d005a9185a45c7e81f490b1a31c52fec5d218
Dima <dimabojko93@gmail.com>
2020-11-05 22:18:12 +0200

    add new file

--- js/script.js
+++ js/script.js
@@ -1,62 +1,30 @@
-let menu = document.querySelector(".menu"),
-	menuItem = document.querySelectorAll(".menu__item"),
-	nav = document.querySelector(".nav"),
-	burgerMenu = document.querySelector(".burger-menu"),
-	burgerMenuItem = document.querySelectorAll(".burger-menu__item");
+"use strict";
 
-document.addEventListener("click", function(event) {
-	if (event.target && event.target.classList.contains("nav") 
-		|| event.target.classList.contains("burger-menu__item") || event.target.classList.contains("burger-menu") ) {
+document.addEventListener("DOMContentLoaded", () => {
 
-		burgerMenuItem[0].classList.toggle("top-transform");
-		burgerMenuItem[1].classList.toggle("center-transform");
-		burgerMenuItem[2].classList.toggle("bottom-bg");
-		nav.classList.toggle("hide");
-	}
-})
-
-//--slider--
-
-let sliderImg = document.querySelectorAll(".slider__img"),
-	afterBtn = document.querySelector(".after-btn"),
-	beforeBtn = document.querySelector(".before-btn");
-
-//--radio-button--
-
-let form = document.querySelector(".form"),
-	wrapper = document.querySelector(".wrapper"),
-	// --Program variables--
-	programWrapper = document.querySelectorAll(".program-wrapper-js"),
-	programText = document.querySelectorAll(".program-text-js"),
-	programInput = document.querySelectorAll(".program-input-js"),
-	circle = document.querySelectorAll(".circle"),
-	// --Extra variables--
-	extraWrapper = document.querySelectorAll(".extra-wrapper-js"),
-	extraText = document.querySelectorAll(".extra-text-js"),
-	extraInput = document.querySelectorAll(".extra-input-js"),
-	checkMark = document.querySelectorAll(".check-mark");
-
-function hideCircle(e) {
-	circle.forEach(item => {
-		item.style.opacity = "0";
-	})
-}
-
-programInput.forEach((item, i) => {
-	item.addEventListener("change", (e) => {
-		hideCircle();
-		if (e.target == item) {
-			circle[i].style.opacity = "1";
+let aboutMe = document.querySelector(".about-me"),
+	foto = document.querySelector(".foto"),
+	fotoImg = document.querySelector(".foto-img"),
+	phone = document.querySelector(".phone"),
+	phoneNumber = document.querySelector(".phone-number"),
+	mail = document.querySelector(".mail"),
+	mailText = document.querySelector(".mail-text");
+console.log(mailText);
+	aboutMe.addEventListener("click", (e) => {
+		console.log(e.target);
+		if (e.target && e.target.classList.contains("phone__icon") 
+					|| e.target.classList.contains("ph")) {
+			phoneNumber.classList.toggle("hide");
+			foto.classList.toggle("foto-change");
+			fotoImg.classList.toggle("foto-img-change");
+		}
+		if (e.target && e.target.classList.contains("mail__icon") 
+					|| e.target.classList.contains("ml")) {
+			mailText.classList.toggle("hide");
+			foto.classList.toggle("foto-change");
+			fotoImg.classList.toggle("foto-img-change");
 		}
 	})
+
+
 })
-
-extraInput.forEach((item, i) => {
-	item.addEventListener("change", (e) => {
-		if (e.target == item) {
-			checkMark[i].classList.toggle("check-mark-hide")
-		}
-	})
-})
-
-
