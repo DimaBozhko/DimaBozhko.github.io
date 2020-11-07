4e76f4afc4910cb879354cd0c4cfffdedec49a80
Dima <dimabojko93@gmail.com>
2020-11-07 12:54:13 +0200

    change project

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
-let aboutMe = document.querySelector(".about-me"),
-	foto = document.querySelector(".foto"),
-	fotoImg = document.querySelector(".foto-img"),
-	phone = document.querySelector(".phone"),
-	phoneNumber = document.querySelector(".phone-number"),
-	mail = document.querySelector(".mail"),
-	mailText = document.querySelector(".mail-text");
-console.log(mailText);
-	aboutMe.addEventListener("click", (e) => {
-		console.log(e.target);
-		if (e.target && e.target.classList.contains("phone__icon") 
-					|| e.target.classList.contains("ph")) {
-			phoneNumber.classList.toggle("hide");
-			foto.classList.toggle("foto-change");
-			fotoImg.classList.toggle("foto-img-change");
-		}
-		if (e.target && e.target.classList.contains("mail__icon") 
-					|| e.target.classList.contains("ml")) {
-			mailText.classList.toggle("hide");
-			foto.classList.toggle("foto-change");
-			fotoImg.classList.toggle("foto-img-change");
-		}
-	})
+// let aboutMe = document.querySelector(".about-me"),
+// 	foto = document.querySelector(".foto"),
+// 	fotoImg = document.querySelector(".foto-img"),
+// 	phone = document.querySelector(".phone"),
+// 	phoneNumber = document.querySelector(".phone-number"),
+// 	mail = document.querySelector(".mail"),
+// 	mailText = document.querySelector(".mail-text");
+// console.log(mailText);
+// 	aboutMe.addEventListener("click", (e) => {
+// 		console.log(e.target);
+// 		if (e.target && e.target.classList.contains("phone__icon") 
+// 					|| e.target.classList.contains("ph")) {
+// 			phoneNumber.classList.toggle("hide");
+// 			foto.classList.toggle("foto-change");
+// 			fotoImg.classList.toggle("foto-img-change");
+// 		}
+// 		if (e.target && e.target.classList.contains("mail__icon") 
+// 					|| e.target.classList.contains("ml")) {
+// 			mailText.classList.toggle("hide");
+// 			foto.classList.toggle("foto-change");
+// 			fotoImg.classList.toggle("foto-img-change");
+// 		}
+// 	})
 
 
-})
+// })
