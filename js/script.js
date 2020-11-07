c418b525977b9713ef479a8a07fd385988097a27
Dima <dimabojko93@gmail.com>
2020-11-07 17:07:56 +0200

    change project

--- js/script.js
+++ js/script.js
@@ -1,30 +1,105 @@
 "use strict";
 
-// document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+	let menu = document.querySelector(".menu"),
+			menuItem = document.querySelectorAll(".menu-item"),
+			icon = document.querySelectorAll(".icon"),
+			iconJs = document.querySelectorAll(".icon-js"),
+			menuItemText = document.querySelectorAll(".menu-item__text"),
+			bg = ["bg-green", "bg-red", "bg-blue"],
+			contentItem = document.querySelectorAll(".content-item");
 
-// let aboutMe = document.querySelector(".about-me"),
-// 	foto = document.querySelector(".foto"),
-// 	fotoImg = document.querySelector(".foto-img"),
-// 	phone = document.querySelector(".phone"),
-// 	phoneNumber = document.querySelector(".phone-number"),
-// 	mail = document.querySelector(".mail"),
-// 	mailText = document.querySelector(".mail-text");
-// console.log(mailText);
-// 	aboutMe.addEventListener("click", (e) => {
-// 		console.log(e.target);
-// 		if (e.target && e.target.classList.contains("phone__icon") 
-// 					|| e.target.classList.contains("ph")) {
-// 			phoneNumber.classList.toggle("hide");
-// 			foto.classList.toggle("foto-change");
-// 			fotoImg.classList.toggle("foto-img-change");
-// 		}
-// 		if (e.target && e.target.classList.contains("mail__icon") 
-// 					|| e.target.classList.contains("ml")) {
-// 			mailText.classList.toggle("hide");
-// 			foto.classList.toggle("foto-change");
-// 			fotoImg.classList.toggle("foto-img-change");
-// 		}
-// 	})
+let num = 0;
+function hideMenuItem(e) {
+	num++;
+	for (let i = 0; i <= 2; i++) {
+		menuItem[i].classList.toggle("menu-item--hide")
+	}
+	menuItem.forEach((item, i) => {
+		if (e.target == item) {
+			item.classList.remove("menu-item--hide");
+			if (num == 1) {
+				item.classList.add(bg[i]);
 
+			} else {
+				item.classList.remove(bg[i]);
+				num = 0;
+			}
+		}
+	})
+	icon.forEach((item, i) => {
+		if (e.target == item) {
+			menuItem[i].classList.remove("menu-item--hide");
+			if (num == 1) {
+				menuItem[i].classList.add(bg[i]);
 
-// })
+			} else {
+				menuItem[i].classList.remove(bg[i]);
+				num = 0;
+			}
+		}
+
+	})
+	menuItemText.forEach((item, i) => {
+		if (e.target == item) {
+			menuItem[i].classList.remove("menu-item--hide");
+			if (num == 1) {
+				menuItem[i].classList.add(bg[i]);
+
+			} else {
+				menuItem[i].classList.remove(bg[i]);
+				num = 0;
+			}
+		}
+	})
+	iconJs.forEach((item, i) => {
+		if (e.target == item) {
+			menuItem[i].classList.remove("menu-item--hide");
+			if (num == 1) {
+				menuItem[i].classList.add(bg[i]);
+
+			} else {
+				menuItem[i].classList.remove(bg[i]);
+				num = 0;
+			}
+		}
+	})
+}
+
+function showContent(e) {
+	menuItem.forEach((item, i) => {
+		if (e.target == item) {
+			contentItem[i].classList.toggle("hide");
+		}
+	})
+	icon.forEach((item, i) => {
+		if (e.target == item) {
+			contentItem[i].classList.toggle("hide");
+		}
+
+	})
+	menuItemText.forEach((item, i) => {
+		if (e.target == item) {
+			contentItem[i].classList.toggle("hide");
+		}
+	})
+	iconJs.forEach((item, i) => {
+		if (e.target == item) {
+			contentItem[i].classList.toggle("hide");
+		}
+	})	
+}
+
+	menu.addEventListener("click", (e) => {
+	console.log(e.target)
+
+		if (e.target && e.target.classList.contains("menu-item") 
+			|| e.target.classList.contains("icon") || e.target.classList.contains("icon-js")  
+			|| e.target.classList.contains("menu-item__text")) {
+
+			hideMenuItem(e);
+			showContent(e);
+		}
+	})
+
+})
